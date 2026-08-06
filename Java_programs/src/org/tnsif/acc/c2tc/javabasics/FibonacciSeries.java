257980ca47e46fae955c7728a84d6d0b56991cef
Abhiram <Abhiram@Lenovo-PC>
2026-08-06 13:07:50 +0530

    first commit

---
+++ Java_programs/src/org/tnsif/acc/c2tc/javabasics/FibonacciSeries.java
@@ -0,0 +1,20 @@
+package org.tnsif.acc.c2tc.javabasics;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+
+        int n = 10;
+        int a = 0, b = 1, c;
+
+        System.out.println("Fibonacci Series:");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+            c = a + b;
+            a = b;
+            b = c;
+        }
+	}
+
+}
